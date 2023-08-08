d3768c24a80892ab4c508f160937f78ed781e824
David Garcia <david_5043@hotmail.com>
2023-08-08 16:05:22 -0600

    first commit

---
+++ utils/get_env_var.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"errors"
+	"os"
+)
+
+func GetEnvVar(envVar string) (string, error) {
+	value := os.Getenv(envVar)
+	if value == "" {
+		return "", errors.New("You must provide " + envVar + " in environment variables")
+	}
+
+	return value, nil
+}
