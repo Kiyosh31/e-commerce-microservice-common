745133167b8cd0cdb01f9cb1eee64d4154872daf
David Garcia <david_5043@hotmail.com>
2023-08-08 19:19:28 -0600

    fixed typo

--- utils/time.go
+++ utils/time.go
@@ -2,7 +2,7 @@ package utils
 
 import "time"
 
-func stringToTimeDuration(definedTime string) (time.Duration, error) {
+func StringToTimeDuration(definedTime string) (time.Duration, error) {
 	duration, err := time.ParseDuration(definedTime)
 	if err != nil {
 		return time.Duration(0), err
