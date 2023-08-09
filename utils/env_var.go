8f0c29d1510af80e7fa1025bcdba931d31d2d49f
David Garcia <david_5043@hotmail.com>
2023-08-08 18:46:18 -0600

    fixing error handling

--- utils/env_var.go
+++ utils/env_var.go
@@ -1,14 +1,15 @@
 package utils
 
 import (
-	"errors"
+	"fmt"
 	"os"
 )
 
 func GetEnvVar(envVar string) (string, error) {
 	value := os.Getenv(envVar)
 	if value == "" {
-		return "", errors.New("You must provide " + envVar + " in environment variables")
+		return "",
+			fmt.Errorf("You must provide" + envVar + "in environment variables")
 	}
 
 	return value, nil
